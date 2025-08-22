003611707c5894c2190602124404eedbedd7b1f8
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-22 12:05:12 +0200

    print first prompt + usage metadata

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
 
 def main():
-    print("Hello from aiagent!")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response tokens: {response.usage_metadata.prompt_token_count}")
+
     
 
 
