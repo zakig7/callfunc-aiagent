12c5c8d984baa28bdf7699891a0b32c47b666408
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-22 12:14:12 +0200

    semantics + bump py minor version

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    
 
 
 if __name__ == "__main__":
