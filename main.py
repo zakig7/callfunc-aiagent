cf7121df31b3e138bd6b85fba0a391629cb64a87
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-23 12:23:51 +0200

    move input to arg

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
+
+if len(sys.argv) != 2:
+    print("Error, please provide your prompt.")
+    sys.exit(1)
 
 
 def main():
     load_dotenv()
+    
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=sys.argv[1]
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
