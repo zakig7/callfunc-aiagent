9e0b8a1e9bc6e0277ed1277e4bc3226600a635c8
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-24 21:50:43 +0200

    better error handling, simplify and clean

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,38 +1,33 @@
 import os
 
+
 def get_files_info(working_directory, directory="."):
-    directory = os.path.join(working_directory, directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    abs_dir = os.path.abspath(working_directory)
 
     """
     IMPORTANT! Without this restriction, the LLM might go running amok anywhere on the machine,
     reading sensitive files or overwriting any data.
     """
 
-    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
+    if not target_dir.startswith(abs_dir):
+        # Using {directory} param here to preserve the user's original input
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    elif os.path.isdir(directory) is False:
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
-    else:
-        is_dir = True
-        output_list = []
-        try:
-            for i in os.listdir(directory):
-                filepath = os.path.join(directory, i)
-                # Assess for every "i" wether file or directory
-                is_dir = os.path.isdir(filepath)
 
-                # Detrmine file size and handle exception
-                filesize = 0
-                if not is_dir:
-                    try:
-                        filesize = os.path.getsize(filepath)
-                    except Exception as e:
-                        return f"Error: Could not get size of {i}: {e}"
-                # Retrun a full formatted string for each i
-                item_string = f"- {i}: file_size={filesize} bytes, is_dir={is_dir}"
-                output_list.append(item_string)
-        except Exception as e:
-            return f"Error:{e}"
-        
-        # Combine all strings in output_list into one final string       
-        return "\n".join(output_list)
+    try:
+        info_list = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            filesize = os.path.getsize(filepath)
+            # Retrun a full formatted string for each filename
+            info_list.append(
+                f"- {filename}: file_size={filesize} bytes, is_dir={is_dir}"
+            )
+    except Exception as e:
+        return f"Error: {e}"
+    
+    # Combine all strings in output_list into one final string       
+    return "\n".join(info_list)
