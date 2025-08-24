ba3b4d8cc75a61e4e663884a14131308d3004b0f
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-24 14:36:41 +0200

    add -- verobose, differntiate prompt from --verbose param

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -14,19 +15,32 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    # Multi-word prompts work naturally
-    user_prompt = " ".join(args)
+    verbose_param = False
+    user_prompt = []
+    for i in args:
+        if i == "--verbose":
+            verbose_param = True
+        else:
+            user_prompt.append(i)
+
+    # Multi-word args into prompt
+    user_prompt = " ".join(user_prompt)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt
+        contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose_param is True:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
