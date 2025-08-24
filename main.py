f7b591298573a8dcc3c269a196df7ef4a4166980
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-24 15:30:51 +0200

    move api call into own func

--- main.py
+++ main.py
@@ -6,41 +6,44 @@ from google.genai import types
 
 def main():
     load_dotenv()
-    
-    # Accepts any number of prompt words as arg
-    args = sys.argv[1:]
+
+    # Differentiate acutal arguments
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('\nUsage: python main.py \"your prompt here\" [--argument]')
+        print('Example: python main.py \"How do I build a calculator app? --verbose\"')
         sys.exit(1)
-    verbose_param = False
-    user_prompt = []
-    for i in args:
-        if i == "--verbose":
-            verbose_param = True
-        else:
-            user_prompt.append(i)
-
-    # Multi-word args into prompt
-    user_prompt = " ".join(user_prompt)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
+
+    user_prompt = " ".join(args)
+
+    # Record user prompts in a list to preserve conversation context
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+
+    generate_content(client, messages, verbose, user_prompt)
+
+def generate_content(client, messages, verbose, user_prompt):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    print(response.text)
-    if verbose_param is True:
+
+    # Verbose flag
+    if verbose:
         print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print(response.text)
 
 
 if __name__ == "__main__":
