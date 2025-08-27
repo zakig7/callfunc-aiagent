958218772cc325a79142e14b0c988a77794cf4ef
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-27 23:54:05 +0200

    semantics, comment, simpler tests

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,27 +1,31 @@
 import os
-import config
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory, file_path):
 
     target_dir = os.path.abspath(os.path.join(working_directory, file_path))
-    abs_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(working_directory)
 
     """
     IMPORTANT! Without this restriction, the LLM might go running amok anywhere on the machine,
     reading sensitive files or overwriting any data.
     """
 
-    if not target_dir.startswith(abs_dir):
+    if not target_dir.startswith(abs_file_path):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(target_dir):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
         with open(target_dir, "r") as f:
-            file_content_string = f.read(config.MAX_CHARS)
+            file_content_string = f.read(MAX_CHARS)
+            # More efficient stream-based for very large files. No need to read
+            # the entire file's metadata just to get its size
             remaining_content = f.read(1)
             if remaining_content:
-                file_content_string += (f'...File "{file_path}" truncated at 10000 characters]')
+                file_content_string += (
+                    f'...File "{file_path}" truncated at 10000 characters]'
+                )
         
     except Exception as e:
         return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -3,20 +3,15 @@ from functions.get_file_content import get_file_content
 
 def test():
     result = get_file_content("calculator", "main.py")
-    print("Result for current directory:")
     print(result)
-    print("")
 
     result = get_file_content("calculator", "pkg/calculator.py")
-    print("\nResult for 'pkg' directory:")
     print(result)
 
     result = get_file_content("calculator", "/bin/cat")
-    print("\nResult for '/bin' directory:")
     print(result)
 
     result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("\nResult for '../' directory:")
     print(result)
 
 
