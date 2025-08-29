5df8a9a6f59afce1260cc9e58ff5d9cc30bc8ab8
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-29 20:45:41 +0200

    prepare tests, archive old ones

--- tests.py
+++ tests.py
@@ -1,15 +1,22 @@
-from functions.write_file_content import write_file
+from functions.run_python import run_python_file
 
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print(result)
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
     print(result)
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "tests.py")
     print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
 
 if __name__ == "__main__":
     test()
