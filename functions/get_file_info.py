499d053675e63a85076361836e48113f11427a77
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-08-31 11:30:07 +0200

    types.FunctionDeclaration to build the "declaration" or "schema"

--- functions/get_file_info.py
+++ functions/get_file_info.py
@@ -1,4 +1,20 @@
 import os
+from google.genai import types
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
 
 def get_file_info(working_directory, directory="."):
